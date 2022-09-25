d87c9bcb6bd7642d621555517665769b3d9ea717
Kavindu Lakmal <80127637+Kavindulakmal@users.noreply.github.com>
2022-09-25 22:37:25 +0530

    Update TUTE01.sql

--- TUTE01.sql
+++ TUTE01.sql
@@ -1,26 +1,31 @@
-a)SELECT d.dno,d.mgr.ename,d.magr.salary
+/*(a)*/
+SELECT d.dno,d.mgr.ename,d.magr.salary
   FROM Dept d
 
-b)SELECT p.pname,p.pdeptt.mgr.ename
+/*(b)*/
+SELECT p.pname,p.pdeptt.mgr.ename
   FROM Proj p
   WHERE p.budget>50000
 
-c)SELECT p.dept.dno,p.dept.dname, SUM(p.budget)
+/*(c)*/
+SELECT p.dept.dno,p.dept.dname, SUM(p.budget)
   FROM Proj p
   GROUP BY p.dept.dno,p.dept.dname
 
-d)SELECT p.dept.mgr.ename
+/*(d)*/
+SELECT p.dept.mgr.ename
   FROM Proj p
   WHERE p.budget=(SELECT MAX(p.budget)
                   FROM Proj p)
 
-
-e)SELECT p.dept.mgr.eno,SUM(p.budget)
+/*(e)*/
+SELECT p.dept.mgr.eno,SUM(p.budget)
   FROM Proj p
   HAVING SUM(p.budget)>60000 
   GROUP BY p.dept.mgr.eno
 
-f)SELECT p.dept.mgr.eno,SUM(p.budget)
+/*(f)*/
+SELECT p.dept.mgr.eno,SUM(p.budget)
   FROM Proj p
   WHERE p.dept.mgr.eno(SELECT p.dept.mgr.eno) 
                        FROM Proj p 
